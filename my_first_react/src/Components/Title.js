08e0d21c78c7186a16af6b4d84d25612630ecff1
Linda-K-daly <68587994+Linda-K-daly@users.noreply.github.com>
2020-09-16 17:07:58 +0200

    title

--- my_first_react/src/Components/Title.js
+++ my_first_react/src/Components/Title.js
@@ -1,17 +1,14 @@
 // je dois retourner un h1 différent de Hello World Konexio
-
-
-import React, {Component }  from 'react';
+import React, { Component } from 'react';
 
 class Title extends Component 
 
 render() {
-    return (
-   
-        <h1> Hello Linda the best developper<h1>
-}
 
-}
+    return(
 
-export default Title
+        <h1> Hello Linda the best developper</h1>
 
+    )
+    }
+    export default Title    
