854764d14ec9a205bfc55e5bd9776131818ee964
Linda-DalyK <linda_abdelhak@hotmail.com>
2020-09-21 00:26:46 +0200

    practice

---
+++ react_practice/src/Components/Title.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Title extends Component {
+
+  render() {
+    return (
+      
+        <h1>Hello Linda the Developper</h1>
+      
+    );
+  }
+}
+
+export default Title;
