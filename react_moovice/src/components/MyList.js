a359f60c79a1e129c19e68c312bd02675c48724d
Linda-DalyK <linda_abdelhak@hotmail.com>
2020-10-13 15:56:00 +0200

    Moovice_7_begin

--- react_moovice/src/components/MyList.js
+++ react_moovice/src/components/MyList.js
@@ -1,28 +1,63 @@
 import React from 'react';
+import Card from './movie/Card.js';
 
 class MyList extends React.Component {
 
-// créer la méthode getFromLocalStorage qui va retourner un array avec des ID pré-enregistrés
-// localStorage.setItem('birthdays', JSON.stringify(['1993-02-02', '2001-09-11']));
-// let bdays = localStorage.getItem('birthdays');
-// bdays = JSON.parse(bdays);
+  // créer la méthode getFromLocalStorage qui va retourner un array avec des ID pré-enregistrés
+  // localStorage.setItem('birthdays', JSON.stringify(['1993-02-02', '2001-09-11']));
+  // let bdays = localStorage.getItem('birthdays');
+  // bdays = JSON.parse(bdays);
 
-constructor() {
-  super();
-  this.getFromLocalStorage = this.getFromLocalStorage.bind(this);
-
-  this.state = {
-      movies: [],
-      moviesId: this.getFromLocalStorage()
-  }
+  constructor() {
+    super();
   
-};
 
-    
+    this.state = {
+      movies: [],
+      movieIds: JSON.parse(localStorage.getItem('my-list'))
+    }
+
+  };
+
+
+  componentDidMount() {
+
+    const theList = (movieId) =>
+      fetch(`http://api.themoviedb.org/3/movie/${movieId}?api_key=134d92c3d72c8501356da2496ace8c7e`)
+        .then(res => res.json())
+        .then(data => data); // data[0] -> we receive an array of one element, so we return the first element directly
+        console.log('salut la liste', theList) 
+
+        
+    // create an array of promises
+    const promises = this.state.movieIds.map(movieId => theList(movieId));
+    console.log("où est le state", this.state.movieIds)
+    // execute all promises
+    Promise.all(promises)
+      .then((theList) => {
+        // receives an array of responses
+        // console.log('montre moi le card du film 1', movieIds[0]);
+        // console.log('montre moi le card du film 2', movieIds[1]);
+      });
+
+      this.setState({
+        movies: theList
+      })
+
+    console.log('ou est la liste?', theList)
+  }
+
+
   render() {
     return (
       <div>
-      My List
+        {this.state.movieIds}
+      <Card 
+      movieTitle={secondMovie.title}
+      movieSynospsis={secondMovie.overview}
+      moviePix={secondMovie.poster_path}
+      movieId= {secondMovie.id}
+      />
       </div>
     );
   }
@@ -30,3 +65,13 @@ constructor() {
 
 
 export default MyList;
+
+
+
+// const animals = ['pigs', 'goats', 'sheep'];
+
+// const count = animals.push('cows');
+// console.log(count);
+// // expected output: 4
+// console.log(animals);
+// // expected output: Array ["pigs", "goats", "sheep", "cows"]
--- react_moovice/src/components/PopularBattle.js
+++ react_moovice/src/components/PopularBattle.js
@@ -1,136 +1,138 @@
 
 import React from 'react';
 import Card from './movie/Card.js';
+import MyList from './MyList.js';
 
 
-class PopularBattle extends React.Component {
+                class PopularBattle extends React.Component {
 
-    constructor() {
-        super();
-        this.choseFilm = this.choseFilm.bind(this);
+                    constructor() {
+                        super();
+                        this.choseFilm = this.choseFilm.bind(this);
 
-        this.state = {
-            currentPage: 1,
-            movies: [],
-            // firstMovie:{}
-            id: 1
-        }
-        
-    };
- 
-
-
-    componentDidMount() {
-        fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=134d92c3d72c8501356da2496ace8c7e')
-            .then(res => res.json())
-            .then(json => {
-                // console.log('hello fetchBattle', json.results)
-                this.setState({
-                    movies: json.results,
-                })
-            });
-    }
+                        this.state = {
+                            currentPage: 1,
+                            movies: [],
+                            // firstMovie:{}
+                            id: 1
+                        }
+                        
+                    };
+                
 
 
-
-    // renderCards() {
-    //     return this.state.movies.map((elem, i) => {
-    //     console.log('je suis index du battle', i)
-    //     console.log('je suis un element du battle', elem)
-    //         return (
-    //             <Card
-    //                 key={i}
-    //                 moviePix={elem.poster_path}
-    //                 movieTitle={elem.title}
-    //                 movieSynopsis={elem.overview}
-    //             >
-    //             </Card>
-    //         )
-    //     })
-
-    // }
-
-    choseFilm (id){
-        console.log('chosefilm ok', id)
-        // let myList = []
-
-        // if(JSON.parse(localStorage.getItem('my-list'))) {
-        //     // console.log('Hello localstorage') apparaît au 2ème clik car au 1er c'est un array vide . au 2è click on pousse la liste dans l'array
-        //     myList = JSON.parse(localStorage.getItem('my-list'))
-        // }
-
-        let myList = JSON.parse(localStorage.getItem('my-list')) || [] 
-
-        if (!myList.includes(id)) {
-            myList.push(id)
-            localStorage.setItem('my-list', JSON.stringify(myList))
-        }
-
-        // console.log('myList', myList)
-
-        this.setState({
-            currentPage: this.state.currentPage + 1
-        })
-    }
-
-
-    render() {
-        const {
-            movies,
-            currentPage,
-        } = this.state
-
-        const secondIndex = currentPage * 2 - 1
-        const firstIndex = secondIndex - 1
-        // console.log('render de PopularBattle', this.state)
-
-        const firstMovie = movies[firstIndex]
-        const secondMovie = movies[secondIndex]
-
-        if (firstMovie === undefined) {
-            return <div>Movie on loading</div>
-        }
-
-        if (secondMovie === undefined) {
-            return <div>Movie on loading</div>
-        }
-
-        console.log('j/affiche lefirstMovie', firstMovie)
-
-        return (
-            <div>
-                {/* {{this.renderCards()}} */}
-                <button onClick= {() => this.choseFilm(firstMovie.id)}>
-                    <Card
-                        movieTitle={firstMovie.title}
-                        movieSynospsis={firstMovie.overview}
-                        moviePix={firstMovie.poster_path}
-                        movieId= {firstMovie.id}
-                    />
-                </button>
-                <button onClick= {() => this.choseFilm(secondMovie.id)}>
-                    <Card
-                        movieTitle={secondMovie.title}
-                        movieSynospsis={secondMovie.overview}
-                        moviePix={secondMovie.poster_path}
-                        movieId= {secondMovie.id}
-                    />
-                </button>
-
-            </div>
+                    componentDidMount() {
+                        fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=134d92c3d72c8501356da2496ace8c7e')
+                            .then(res => res.json())
+                            .then(json => {
+                                // console.log('hello fetchBattle', json.results)
+                                this.setState({
+                                    movies: json.results,
+                                })
+                            });
+                    }
 
 
 
+                    // renderCards() {
+                    //     return this.state.movies.map((elem, i) => {
+                    //     console.log('je suis index du battle', i)
+                    //     console.log('je suis un element du battle', elem)
+                    //         return (
+                    //             <Card
+                    //                 key={i}
+                    //                 moviePix={elem.poster_path}
+                    //                 movieTitle={elem.title}
+                    //                 movieSynopsis={elem.overview}
+                    //             >
+                    //             </Card>
+                    //         )
+                    //     })
 
-        );
+                    // }
+
+                    choseFilm (id){
+                        console.log('chosefilm ok', id)
+                        // let myList = []
+
+                        // if(JSON.parse(localStorage.getItem('my-list'))) {
+                        //     // console.log('Hello localstorage') apparaît au 2ème clik car au 1er c'est un array vide . au 2è click on pousse la liste dans l'array
+                        //     myList = JSON.parse(localStorage.getItem('my-list'))
+                        // }
+
+                        let myList = JSON.parse(localStorage.getItem('my-list')) || [] 
+
+                        if (!myList.includes(id)) {
+                            myList.push(id)
+                            localStorage.setItem('my-list', JSON.stringify(myList))
+                        }
+
+                        // console.log('myList', myList)
+
+                        this.setState({
+                            currentPage: this.state.currentPage + 1
+                        })
+                    }
 
 
-    }
+                    render() {
+                        const {
+                            movies,
+                            currentPage,
+                        } = this.state
+
+                        const secondIndex = currentPage * 2 - 1
+                        const firstIndex = secondIndex - 1
+                        // console.log('render de PopularBattle', this.state)
+
+                        const firstMovie = movies[firstIndex]
+                        const secondMovie = movies[secondIndex]
+
+                        if (firstMovie === undefined) {
+                            return <div>Movie on loading</div>
+                        }
+
+                        if (secondMovie === undefined) {
+                            return <div>Movie on loading</div>
+                        }
+
+                        console.log('j/affiche lefirstMovie', firstMovie)
+
+                        return (
+                            <div>
+                                {/* {{this.renderCards()}} */}
+                                <button onClick= {() => this.choseFilm(firstMovie.id)}>
+                                    <Card
+                                        movieTitle={firstMovie.title}
+                                        movieSynospsis={firstMovie.overview}
+                                        moviePix={firstMovie.poster_path}
+                                        movieId= {firstMovie.id}
+                                    />
+                                </button>
+                                <button onClick= {() => this.choseFilm(secondMovie.id)}>
+                                    <Card
+                                        movieTitle={secondMovie.title}
+                                        movieSynospsis={secondMovie.overview}
+                                        moviePix={secondMovie.poster_path}
+                                        movieId= {secondMovie.id}
+                                    />
+                                </button>
+                                    <MyList choseFilm={() => this.choseFilm()}/>
+
+                            </div>
 
 
 
 
+                        );
 
-}
+
+                    }
+
+
+
+
+
+                }
 
 export default PopularBattle;
