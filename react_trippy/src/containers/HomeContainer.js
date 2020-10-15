5b1447e1837f332ced134a5c93b85149cfb3d54a
Linda-DalyK <linda_abdelhak@hotmail.com>
2020-10-15 16:56:01 +0200

    trippy_begin

---
+++ react_trippy/src/containers/HomeContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class HomeContainer extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hello Home</p>
+      </div>
+    );
+  }
+}
+
+export default HomeContainer;
